13ea96ba2be34a719904c496818a7f3cb0444982
DavidFranco3 <josedavidayalafranco3@gmail.com>
2022-11-21 00:20:25 -0600

    reestructuracion  y  mejoras en el codigo

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -53,17 +53,6 @@ router.get("/totalVentas", async (_req, res) => {
 });
 
 // Listar paginando las ventas
-router.get("/totalVentasDia", async (req, res) => {
-    const { dia } = req.query;
-    await ventas
-        .find({ estado: "true", createdAt: { $gte: new Date(dia + 'T00:00:00.000Z'), $lte: new Date(dia + 'T23:59:59.999Z') } })
-        .count()
-        .sort({ _id: -1 })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
-
-// Listar paginando las ventas
 router.get("/listarPaginandoDia", async (req, res) => {
     const { pagina, limite, dia } = req.query;
     //console.log("Pagina ", pagina , " Limite ", limite)
@@ -93,71 +82,62 @@ router.get("/listarTotalVentasDia", async (req, res) => {
             let sandwiches_vendidos = 0;
             let desayunos_vendidos = 0;
             let envios_vendidos = 0;
-            let total = 0;
             let efectivo = 0;
             let tarjeta = 0;
             let transferencia = 0;
             map(data, (totales, indexPrincipal) => {
 
-                if (totales.estado === "true") {
-                    // Sumatoria de artículos vendidos
-                    map(totales.productos, (producto, index) => {
-                        if (producto.categoria === "623b7f2fe94614410f810a47" || producto.categoria === "623b7f24e94614410f810a44" || producto.categoria === "623b7f1be94614410f810a41" || producto.categoria === "623b7f0be94614410f810a3e") {
-                            productos_vendidos += 1;
-                        }
-                        if (producto.categoria === "62397cbaf67f5c7c54a0560b") {
-                            bebidas_vendidas += 1;
-                        }
-                        if (producto.categoria === "625d8b798bb242960f22ae56") {
-                            extras_vendidos += 1;
-                        }
-                        if (producto.categoria === "627a86119d083324e156bda8") {
-                            sandwiches_vendidos += 1;
-                        }
-                        if (producto.categoria === "631b541c05ab0cb5a3c1dbd6") {
-                            desayunos_vendidos += 1;
-                        }
-                        if (producto.categoria === "6262ee08e1383d22020db083") {
-                            envios_vendidos += 1;
-                        }
-                    })
-                }
-
-                // Sumatoria del total de ventas
-                if (totales.estado === "true") {
-                    total += parseFloat(totales.total);
-                    // console.log(totales.total)
-                }
+                // Sumatoria de artículos vendidos
+                map(totales.productos, (producto, index) => {
+                    if (producto.categoria === "623b7f2fe94614410f810a47" || producto.categoria === "623b7f24e94614410f810a44" || producto.categoria === "623b7f1be94614410f810a41" || producto.categoria === "623b7f0be94614410f810a3e") {
+                        productos_vendidos += 1;
+                    }
+                    if (producto.categoria === "62397cbaf67f5c7c54a0560b") {
+                        bebidas_vendidas += 1;
+                    }
+                    if (producto.categoria === "625d8b798bb242960f22ae56") {
+                        extras_vendidos += 1;
+                    }
+                    if (producto.categoria === "627a86119d083324e156bda8") {
+                        sandwiches_vendidos += 1;
+                    }
+                    if (producto.categoria === "631b541c05ab0cb5a3c1dbd6") {
+                        desayunos_vendidos += 1;
+                    }
+                    if (producto.categoria === "6262ee08e1383d22020db083") {
+                        envios_vendidos += 1;
+                    }
+                })
 
                 // Sumatoria de ventas realizadas con efectivo
-                if (totales.estado === "true" && totales.tipoPago === "Efectivo") {
+                if (totales.tipoPago === "Efectivo") {
                     efectivo += parseFloat(totales.total);
                     // console.log(totales.total)
                 }
 
                 // Sumatoria de ventas realizadas con tarjeta
-                if (totales.estado === "true" && totales.tipoPago === "Tarjeta") {
+                if (totales.tipoPago === "Tarjeta") {
                     tarjeta += parseFloat(totales.total);
                     // console.log(totales.total)
                 }
 
                 // Sumatoria de ventas realizadas con transferencia
-                if (totales.estado === "true" && totales.tipoPago === "Transferencia") {
+                if (totales.tipoPago === "Transferencia") {
                     transferencia += parseFloat(totales.total);
                     // console.log(totales.total)
                 }
 
             })
-            res.status(200).json({ efectivo: efectivo, tarjeta: tarjeta, transferencia: transferencia, total: total, tortasVendidas: productos_vendidos, bebidasVendidas: bebidas_vendidas, extrasVendidos: extras_vendidos, sandwichesVendidos: sandwiches_vendidos, desayunosVendidos: desayunos_vendidos, enviosVendidos: envios_vendidos })
+            res.status(200).json({ efectivo: efectivo, tarjeta: tarjeta, transferencia: transferencia, tortasVendidas: productos_vendidos, bebidasVendidas: bebidas_vendidas, extrasVendidos: extras_vendidos, sandwichesVendidos: sandwiches_vendidos, desayunosVendidos: desayunos_vendidos, enviosVendidos: envios_vendidos })
         })
         .catch((error) => res.json({ message: error }));
 });
 
 // Obtener las ventas de un dia en especifico
 router.get("/listarTotalVentasMes", async (req, res) => {
-    const { dia } = req.query;
+    const { mes } = req.query;
     await ventas
-        .find({ estado: "true", agrupar: dia })
+        .find({ estado: "true", agrupar: mes })
         .sort({ _id: -1 })
         .then((data) => {
             //console.log(data)
@@ -167,62 +147,53 @@ router.get("/listarTotalVentasMes", async (req, res) => {
             let sandwiches_vendidos = 0;
             let desayunos_vendidos = 0;
             let envios_vendidos = 0;
-            let total = 0;
             let efectivo = 0;
             let tarjeta = 0;
             let transferencia = 0;
             map(data, (totales, indexPrincipal) => {
 
-                if (totales.estado === "true") {
-                    // Sumatoria de artículos vendidos
-                    map(totales.productos, (producto, index) => {
-                        if (producto.categoria === "623b7f2fe94614410f810a47" || producto.categoria === "623b7f24e94614410f810a44" || producto.categoria === "623b7f1be94614410f810a41" || producto.categoria === "623b7f0be94614410f810a3e") {
-                            productos_vendidos += 1;
-                        }
-                        if (producto.categoria === "62397cbaf67f5c7c54a0560b") {
-                            bebidas_vendidas += 1;
-                        }
-                        if (producto.categoria === "625d8b798bb242960f22ae56") {
-                            extras_vendidos += 1;
-                        }
-                        if (producto.categoria === "627a86119d083324e156bda8") {
-                            sandwiches_vendidos += 1;
-                        }
-                        if (producto.categoria === "631b541c05ab0cb5a3c1dbd6") {
-                            desayunos_vendidos += 1;
-                        }
-                        if (producto.categoria === "6262ee08e1383d22020db083") {
-                            envios_vendidos += 1;
-                        }
-                    })
-                }
-
-                // Sumatoria del total de ventas
-                if (totales.estado === "true") {
-                    total += parseFloat(totales.total);
-                    // console.log(totales.total)
-                }
+                // Sumatoria de artículos vendidos
+                map(totales.productos, (producto, index) => {
+                    if (producto.categoria === "623b7f2fe94614410f810a47" || producto.categoria === "623b7f24e94614410f810a44" || producto.categoria === "623b7f1be94614410f810a41" || producto.categoria === "623b7f0be94614410f810a3e") {
+                        productos_vendidos += 1;
+                    }
+                    if (producto.categoria === "62397cbaf67f5c7c54a0560b") {
+                        bebidas_vendidas += 1;
+                    }
+                    if (producto.categoria === "625d8b798bb242960f22ae56") {
+                        extras_vendidos += 1;
+                    }
+                    if (producto.categoria === "627a86119d083324e156bda8") {
+                        sandwiches_vendidos += 1;
+                    }
+                    if (producto.categoria === "631b541c05ab0cb5a3c1dbd6") {
+                        desayunos_vendidos += 1;
+                    }
+                    if (producto.categoria === "6262ee08e1383d22020db083") {
+                        envios_vendidos += 1;
+                    }
+                })
 
                 // Sumatoria de ventas realizadas con efectivo
-                if (totales.estado === "true" && totales.tipoPago === "Efectivo") {
+                if (totales.tipoPago === "Efectivo") {
                     efectivo += parseFloat(totales.total);
                     // console.log(totales.total)
                 }
 
                 // Sumatoria de ventas realizadas con tarjeta
-                if (totales.estado === "true" && totales.tipoPago === "Tarjeta") {
+                if (totales.tipoPago === "Tarjeta") {
                     tarjeta += parseFloat(totales.total);
                     // console.log(totales.total)
                 }
 
                 // Sumatoria de ventas realizadas con transferencia
-                if (totales.estado === "true" && totales.tipoPago === "Transferencia") {
+                if (totales.tipoPago === "Transferencia") {
                     transferencia += parseFloat(totales.total);
                     // console.log(totales.total)
                 }
 
             })
-            res.status(200).json({ efectivo: efectivo, tarjeta: tarjeta, transferencia: transferencia, total: total, tortasVendidas: productos_vendidos, bebidasVendidas: bebidas_vendidas, extrasVendidos: extras_vendidos, sandwichesVendidos: sandwiches_vendidos, desayunosVendidos: desayunos_vendidos, enviosVendidos: envios_vendidos })
+            res.status(200).json({ efectivo: efectivo, tarjeta: tarjeta, transferencia: transferencia, tortasVendidas: productos_vendidos, bebidasVendidas: bebidas_vendidas, extrasVendidos: extras_vendidos, sandwichesVendidos: sandwiches_vendidos, desayunosVendidos: desayunos_vendidos, enviosVendidos: envios_vendidos })
         })
         .catch((error) => res.json({ message: error }));
 });
@@ -243,13 +214,13 @@ router.get("/listarDetallesVentasMes", async (req, res) => {
 
 // Listar paginando las ventas
 router.get("/listarPaginandoMes", async (req, res) => {
-    const { pagina, limite, dia } = req.query;
+    const { pagina, limite, mes } = req.query;
     //console.log("Pagina ", pagina , " Limite ", limite)
 
     const skip = (pagina - 1) * limite;
 
     await ventas
-        .find({ estado: "true", agrupar: dia })
+        .find({ estado: "true", agrupar: mes })
         .sort({ _id: -1 })
         .skip(skip)
         .limit(limite)
@@ -316,56 +287,6 @@ router.get("/listarDetallesProductosVendidosMes", async (req, res) => {
         })
         .catch((error) => res.json({ message: error }));
 });
-
-// Obtener total de productos vendidos, indicando usuario y fecha
-router.get("/listarDetallesUsuario", async (req, res) => {
-    const { usuario, dia } = req.query;
-    //console.log(dia)
-    await ventas
-        .find({ createdAt: { $gte: new Date(dia + 'T00:00:00.000Z'), $lte: new Date(dia + 'T23:59:59.999Z') }, usuario: usuario })
-        .sort({ _id: -1 })
-        .then((data) => {
-            //console.log(data)
-            let productos_vendidos = 0;
-            let total = 0;
-            let efectivo = 0;
-            let tarjeta = 0;
-            map(data, (totales, indexPrincipal) => {
-
-                if (totales.estado === "true") {
-                    // Sumatoria de artículos vendidos
-                    map(totales.productos, (producto, index) => {
-                        if (producto.categoria === "623b7f2fe94614410f810a47" || producto.categoria === "623b7f24e94614410f810a44" || producto.categoria === "623b7f1be94614410f810a41" || producto.categoria === "623b7f0be94614410f810a3e") {
-                            productos_vendidos += 1;
-                        }
-                    })
-                }
-
-                // Sumatoria del total de ventas
-                if (totales.estado === "true") {
-                    total += parseFloat(totales.total)
-                    // console.log(totales.total)
-                }
-
-                // Sumatoria de ventas realizadas con efectivo
-                if (totales.estado === "true" && totales.tipoPago === "Efectivo") {
-                    efectivo += parseFloat(totales.total)
-                    // console.log(totales.total)
-                }
-
-                // Sumatoria de ventas realizadas con tarjeta
-                if (totales.estado === "true" && totales.tipoPago === "Tarjeta") {
-                    tarjeta += parseFloat(totales.total)
-                    // console.log(totales.total)
-                }
-
-            })
-            res.status(200).json({ efectivo: efectivo, tarjeta: tarjeta, total: total, tortasVendidas: productos_vendidos })
-        })
-        .catch((error) => res.json({ message: error }));
-});
-//
-
 
 // Obtener una venta en especifico
 router.get("/obtener/:id", async (req, res) => {
